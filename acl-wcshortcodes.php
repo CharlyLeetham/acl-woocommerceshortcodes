3f273adfcc32d68ac4e76701d3158e0bb34492af
CharlyLeetham <helpdesk@askcharlyleetham.com>
2023-11-20 12:36:48 -1000

    ww

--- acl-wcshortcodes.php
+++ acl-wcshortcodes.php
@@ -73,6 +73,62 @@ Version 1.0 - Original Version
 		return ob_get_clean();
 	}
 
+
+	function acl_add_to_cart_text() {
+		return apply_filters( 'woocommerce_product_add_to_cart_text', $this->is_purchasable() ? __( 'Select options', 'woocommerce' ) : __( 'Read more', 'woocommerce' ), $this );
+	}
+
+
+
+	if ( ! function_exists( 'woocommerce_template_loop_add_to_cart' ) ) {
+
+		/**
+		 * Get the add to cart template for the loop.
+		 *
+		 * @param array $args Arguments.
+		 */
+		function woocommerce_template_loop_add_to_cart( $args = array() ) {
+			global $product;
+	
+			if ( $product ) {
+				$defaults = array(
+					'quantity'   => 1,
+					'class'      => implode(
+						' ',
+						array_filter(
+							array(
+								'button',
+								wc_wp_theme_get_element_class_name( 'button' ), // escaped in the template.
+								'product_type_' . $product->get_type(),
+								$product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
+								$product->supports( 'ajax_add_to_cart' ) && $product->is_purchasable() && $product->is_in_stock() ? 'ajax_add_to_cart' : '',
+							)
+						)
+					),
+					'attributes' => array(
+						'data-product_id'  => $product->get_id(),
+						'data-product_sku' => $product->get_sku(),
+						'aria-label'       => $product->add_to_cart_description(),
+						'aria-describedby' => $product->add_to_cart_aria_describedby(),
+						'rel'              => 'nofollow',
+					),
+				);
+	
+				$args = apply_filters( 'woocommerce_loop_add_to_cart_args', wp_parse_args( $args, $defaults ), $product );
+	
+				if ( ! empty( $args['attributes']['aria-describedby'] ) ) {
+					$args['attributes']['aria-describedby'] = wp_strip_all_tags( $args['attributes']['aria-describedby'] );
+				}
+	
+				if ( isset( $args['attributes']['aria-label'] ) ) {
+					$args['attributes']['aria-label'] = wp_strip_all_tags( $args['attributes']['aria-label'] );
+				}
+	
+				wc_get_template( 'loop/add-to-cart.php', $args );
+			}
+		}
+	}	
+
 	function acl_test_remove() {
 		remove_shortcode ( 'add_to_cart' );
 		add_shortcode ( 'add_to_cart', 'acl_add_to_cart' );
